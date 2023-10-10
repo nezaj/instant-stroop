c9a64d0d1b973c521dab1784db3ded771994a2b0
Joe Averbukh <joeaverbukh@gmail.com>
2023-10-10 14:37:28 -0400

    Add single player scoring

--- src/components/scenes/Singleplayer.js
+++ src/components/scenes/Singleplayer.js
@@ -1,8 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 import SafeView from "../shared/SafeView";
 
+function chooseRandomColor() {
+  const colors = ["red", "green", "blue", "yellow"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+const colorMap = {
+  "text-red-400": { color: "rgb(248 113 113)" },
+  "text-green-400": { color: "rgb(74 222 128)" },
+  "text-blue-400": { color: "rgb(96 165 250)" },
+  "text-yellow-400": { color: "rgb(250 204 21)" },
+};
+
 function Screen({ data }) {
+  const [score, setScore] = useState(0);
+  const [label, setLabel] = useState(chooseRandomColor());
+  const [color, setColor] = useState(chooseRandomColor());
+  const textColor = `text-${color}-400`;
+
+  const onPress = (sqColor) => {
+    if (sqColor == label) {
+      setScore((prevScore) => prevScore + 1);
+      setLabel(chooseRandomColor());
+      setColor(chooseRandomColor());
+    } else {
+      setScore((prevScore) => Math.max(prevScore - 2, 0));
+    }
+  };
+
   return (
     <SafeView className="flex-1">
       {/* Top Bar */}
@@ -11,20 +40,37 @@ function Screen({ data }) {
           <Text className="font-bold text-xl">Time: 30</Text>
           <Text className="font-bold text-xl">Best: 25</Text>
         </View>
-        <Text className="font-bold text-5xl">8</Text>
+        <Text className="font-bold text-5xl">{score}</Text>
       </View>
 
       {/* Color Label */}
       <View className="flex-1 justify-center items-center">
-        <Text className="font-bold text-5xl">RED</Text>
+        <Text
+          style={colorMap[textColor]}
+          className="font-bold text-5xl uppercase"
+        >
+          {label}
+        </Text>
       </View>
 
       {/* Grid Boxes */}
       <View className="flex-1 flex-row flex-wrap justify-center mx-8">
-        <View className="w-32 h-32 bg-gray-200 m-1"></View>
-        <View className="w-32 h-32 bg-gray-300 m-1"></View>
-        <View className="w-32 h-32 bg-gray-400 m-1"></View>
-        <View className="w-32 h-32 bg-gray-500 m-1"></View>
+        <TouchableOpacity
+          onPress={() => onPress("red")}
+          className="w-32 h-32 bg-red-400 m-1"
+        ></TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => onPress("green")}
+          className="w-32 h-32 bg-green-400 m-1"
+        ></TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => onPress("blue")}
+          className="w-32 h-32 bg-blue-400 m-1"
+        ></TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => onPress("yellow")}
+          className="w-32 h-32 bg-yellow-400 m-1"
+        ></TouchableOpacity>
       </View>
     </SafeView>
   );
