dad79ffc0f73027b25c3be122f21247e0f21228a
Joe Averbukh <joeaverbukh@gmail.com>
2023-10-10 11:01:39 -0400

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { init, useQuery, transact, tx } from "@instantdb/react-native";
 import { View, Text, Linking, Button } from "react-native";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 const APP_ID = "24b522b3-0ef8-4939-9646-658aac8716af";
 
@@ -9,64 +13,33 @@ init({
 });
 
 function App() {
-  const { isLoading, error, data } = useQuery({ colors: {} });
-
-  if (isLoading) {
-    return (
-      <View className="p-10">
-        <Text>
-          If you are seeing this you likely need to replace{" "}
-          <Text className="bold">APP_ID</Text> on line 4
-        </Text>
-        <Text>{"\n"}</Text>
-        <Text>
-          You can get your APP_ID by{" "}
-          <Text
-            style={{ color: "blue" }}
-            onPress={() => Linking.openURL("https://instantdb.com/dash")}
-          >
-            logging into your Instant dashboard
-          </Text>
-          . After replacing the id you may need to reload the page.
-        </Text>
-      </View>
-    );
-  }
+  const { isLoading, error, data } = useQuery({});
+  if (isLoading) return <Text>...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
 
-  return <Main data={data} />;
+  return (
+    <SafeAreaProvider>
+      <Main data={data} />
+    </SafeAreaProvider>
+  );
 }
 
-const selectId = "4d39508b-9ee2-48a3-b70d-8192d9c5a059";
-
 function Main({ data }) {
-  const { colors } = data;
-  const { color } = colors[0] || { color: "grey" };
+  const insets = useSafeAreaInsets();
 
   return (
     <View
       className="flex-1 justify-center items-center"
-      style={{ backgroundColor: color }}
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}
     >
-      <View className="my-4">
-        <Text className="text-2xl font-bold mb-4">
-          Hi! pick your favorite color
-        </Text>
-        <View className="flex-row justify-between mx-8">
-          {["green", "blue", "purple"].map((c) => {
-            return (
-              <Button
-                title={c}
-                onPress={() => {
-                  transact(tx.colors[selectId].update({ color: c }));
-                }}
-                key={c}
-              />
-            );
-          })}
-        </View>
-      </View>
+      <Text>Hello World!</Text>
     </View>
   );
 }
+
 export default App;
