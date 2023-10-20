ec1254904d4e0675fa26c6c58e783f4061ea2b99
Joe Averbukh <joeaverbukh@gmail.com>
2023-10-20 07:33:09 -0400

    Better navigation header

--- src/Navigator.js
+++ src/Navigator.js
@@ -23,14 +23,32 @@ const DEFAULT_SCENE = "Main";
 const Stack = createStackNavigator();
 export default function Navigator({ user }) {
   return (
-    <Stack.Navigator initialRouteName={DEFAULT_SCENE}>
-      <Stack.Screen component={Main} initialParams={{ user }} name="Main" />
+    <Stack.Navigator
+      initialRouteName={DEFAULT_SCENE}
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#7c3aed",
+        },
+        headerTintColor: "#facc15",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        options={{ headerShown: false }}
+        component={Main}
+        initialParams={{ user }}
+        name="Main"
+      />
       <Stack.Screen
         name="Singleplayer"
+        options={{ headerShown: false }}
         initialParams={{ user }}
         component={Singleplayer}
       />
       <Stack.Screen
+        options={{ headerShown: false }}
         name="GameOverSingleplayer"
         initialParams={{ user }}
         component={GameOverSingleplayer}
@@ -38,15 +56,20 @@ export default function Navigator({ user }) {
       <Stack.Screen
         name="WaitingRoom"
         initialParams={{ user }}
-        options={({ route }) => ({ title: route.params.roomCode })}
+        options={({ route }) => ({
+          title: route.params.roomCode,
+          headerBackTitle: "Leave",
+        })}
         component={WaitingRoom}
       />
       <Stack.Screen
+        options={{ headerShown: false }}
         name="Multiplayer"
         initialParams={{ user }}
         component={Multiplayer}
       />
       <Stack.Screen
+        options={{ headerShown: false }}
         name="GameOverMultiplayer"
         initialParams={{ user }}
         component={GameOverMultiplayer}
@@ -54,16 +77,19 @@ export default function Navigator({ user }) {
       <Stack.Screen
         name="JoinRoom"
         initialParams={{ user }}
+        options={{ title: "Join Room", headerBackTitle: "Back" }}
         component={JoinRoom}
       />
       <Stack.Screen
         name="HowToPlay"
         initialParams={{ user }}
+        options={{ title: "How to Play", headerBackTitle: "Back" }}
         component={HowToPlay}
       />
       <Stack.Screen
         name="Settings"
         initialParams={{ user }}
+        options={{ title: "Set Name", headerBackTitle: "Back" }}
         component={Settings}
       />
     </Stack.Navigator>
