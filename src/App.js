66f6871dabb3fbc42715a9ff964814c4f2b500a5
Joe Averbukh <joeaverbukh@gmail.com>
2023-10-23 14:07:35 -0400

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { View, Text, Linking, Button } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import React, { useState, useEffect } from "react";
